8ee76a82956145e64561062fa61695d861aa9f86
Jon <jharder@kipsu.com>
2022-12-17 11:53:37 -0600

    Added amount to recipe ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_160924) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_17_173539) do
   create_table "ingredients", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "in_stock", default: true, null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_160924) do
     t.integer "ingredient_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "amount"
     t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
     t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@ ingredients = Ingredient.create(
   ]
 )
 Ingredient.create(name: "Chick Peas", in_stock: false)
+
 recipe = Recipe.create(name: "Butter Chicken")
 recipe.ingredients = ingredients
+
+recipe.ingredients.find_by(name: "Butter").set_amount_for(recipe, "2 Tablespoons")
+recipe.ingredients.find_by(name: "Salt").set_amount_for(recipe, "2 Teaspoons")
+recipe.ingredients.find_by(name: "Garam Masala").set_amount_for(recipe, "1 Tablespoon")
+recipe.ingredients.find_by(name: "Chicken").set_amount_for(recipe, "1 Pound")
